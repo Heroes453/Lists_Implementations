efb89d18a0c517ac9a42b238e0b7be36da3d24de
Heroes453 <152666526+Heroes453@users.noreply.github.com>
2025-11-27 09:31:51 -0500

    Create Array_list.c

---
+++ Array_list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Array_list.h"
+
+#define DEFAULT_CAP 10
+#define GROW_FACT 2
+
+Arrlist* createlist(int capacity, int size){
+	if (capacity <= 0){
+		capacity = DEFAULT_CAP;
+	}
+}
+
+	
+Arrlist* list=(Arrlist*)malloc(sizeof
